375dabed9974068b37751c5e3d11641645726050
Ubuntu <ubuntu@ip-172-31-40-248.us-west-2.compute.internal>
2013-09-03 14:44:36 +0000

    Train on all variables over the last 10 years

--- solar_inc.R
+++ solar_inc.R
@@ -1,5 +1,5 @@
 # Environment definitions for the solar energy forecast program
 train_file<-"~/solar/train/train.csv"
 predictor_file<-"~/solar/train/important_predictors.csv"
-path<-"~/solar/train"
+path<-"~/solar/train/"
 station_indices_file<-"~/solar/station_indices.RData"
--- train_solar.R
+++ train_solar.R
@@ -4,6 +4,7 @@ library(randomForest)
 source("~/solar/solar_inc.R")
 
 solar_energy<-read.csv(train_file)
+total_duration<-length(solar_energy$ACME)
 load(station_indices_file)
 DIM1<-5
 DIM2<-5
@@ -17,12 +18,14 @@ for(i in 1:var) {
 }
 # Get dimensions
 load(pred_files[1],verbose=TRUE)
-ticks<-365*5
+ticks<-3650
 pred_cnt<-DIM1*DIM2*DIM3
 # Keep all forecast horizons for tmp
 pred_cnt_tmp<-pred_cnt*5
 ncol<-pred_cnt*12+pred_cnt_tmp*2
 predictors<-matrix(,nrow=ticks,ncol=ncol)
+start<-(total_duration-ticks+1)
+stop<-total_duration
 ii<-0
 for(i in 1:var) {
    load(pred_files[i],verbose=TRUE)
@@ -37,18 +40,19 @@ for(i in 1:var) {
    }else {
       ncvar_pred2<-aperm(ncvar_pred,c(4,1,2,5,3))
    }
-   for(j in 1:ticks) {
+   jj<-1
+   for(j in start:stop) {
       col<-1
       for(k in (stations_indices[1,2]-2):(stations_indices[1,2]+2)) {
           for(l in (stations_indices[1,1]-2):(stations_indices[1,1]+2)) {
               for(m in 1:DIM3) { 
 # Special handling of tmp values  
                  if (!tmp) {           
-                    predictors[j,ii+col]<-ncvar_pred2[j,k,l,m]
+                    predictors[jj,ii+col]<-ncvar_pred2[j,k,l,m]
                     col<-col+1
                  } else {
                     for(n in 1:5) {
-                        predictors[j,ii+col]<-ncvar_pred2[j,k,l,m,n]
+                        predictors[jj,ii+col]<-ncvar_pred2[j,k,l,m,n]
                         col<-col+1
                     }
                  }
@@ -56,6 +60,7 @@ for(i in 1:var) {
               }
           }
       }
+      jj<-jj+1
    }
    if (tmp) {
       ii<-ii+pred_cnt_tmp
@@ -63,10 +68,10 @@ for(i in 1:var) {
       ii<-ii+pred_cnt
    }
 }
-save(predictors,file=paste(path,"solar_energy_predictors_1.RData"))
+save(predictors,file=paste(path,"solar_energy_predictors_3.RData"))
 
-RF<-randomForest(predictors,solar_energy$ACME[1:ticks],ntree=ticks*10,importance=TRUE,localImp=TRUE)
-save(RF,file="~/solar/RF.RData")
-mae<-sum(abs(RF$predicted-solar_energy$ACME[1:ticks]))/ticks
+RF<-randomForest(predictors,solar_energy$ACME[start:stop],ntree=ticks*10,importance=TRUE,localImp=TRUE)
+save(RF,file="~/solar/RF_3.RData")
+mae<-sum(abs(RF$predicted-solar_energy$ACME[start:stop]))/ticks
 mae
   
